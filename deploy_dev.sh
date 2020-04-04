6b23f0254dfaa3af9758626b3dd3762df02d9a76
Guillermo Alvarado - Sentinel.la <guillermoalvarado89@gmail.com>
2020-04-03 18:05:25 -0600

    deploy_dev nginx option

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -12,11 +12,28 @@ then
 
 	docker run -d --name covidmex-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=my-secret-pw mysql:8.0 mysqld --default-authentication-plugin=mysql_native_password
 	docker run -d --name covidmex --link covidmex-mysql:covidmex-mysql -e MYSQL_USER=root -e MYSQL_PASSWORD=my-secret-pw -e MYSQL_HOST=covidmex-mysql -p 80:80 -e MYSQL_DB=covidmex_db -e COVIDMEX_ENVIRONMENT=development -e COVIDMEX_ADMIN_PASSWORD=d9f8ej3w -e COVIDMEX_ADMIN_USER=admin  covidmex:latest
-	 while ! docker exec covidmex-mysql mysqladmin --user=root --password=y-secret-pw --host "127.0.0.1" ping --silent &> /dev/null ; do
+	while ! docker exec covidmex-mysql mysqladmin --user=root --password=y-secret-pw --host "127.0.0.1" ping --silent &> /dev/null ; do
 	    echo "Waiting for database connection..."
 	    sleep 2
 	done
 	docker exec -it covidmex python manage.py initdb
+	docker ps
+	exit 0
+fi 
+
+if [ "$1" == "--nginx" ]
+then
+	docker stop covidmex
+	docker rm covidmex
+
+	docker stop covidmex-nginx
+	docker rm covidmex-nginx
+
+	docker build -t covidmex:latest -f Dockerfile.covidmex .
+	docker build -t covidmex-nginx:latest -f Dockerfile.nginx .
+
+	docker run -d --name covidmex --link covidmex-mysql:covidmex-mysql -e MYSQL_USER=root -e MYSQL_PASSWORD=my-secret-pw -e MYSQL_HOST=covidmex-mysql  -e MYSQL_DB=covidmex_db -e COVIDMEX_ENVIRONMENT=development -e COVIDMEX_ADMIN_PASSWORD=d9f8ej3w -e COVIDMEX_ADMIN_USER=admin  covidmex:latest
+	docker run -d --name  covidmex-nginx --link covidmex:covidmex -p 80:80 covidmex-nginx:latest
 	docker ps
 	exit 0
 fi 
